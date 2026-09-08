10b67c5269a533dcabf95d817393028a1dac5642
H3reuxx <albanrozes@gmail.com>
2026-09-08 15:08:32 +0200

    premier tp

---
+++ TP1-Lecture-Ecriture-Java/main.java
@@ -0,0 +1,15 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String enTete = "P3\n100 100\n255\n";
+
+        try {
+            FileWriter fw = new FileWriter("firstPPM_withFileWriter.ppm");
+            fw.write(enTete); // écriture du texte directement
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
